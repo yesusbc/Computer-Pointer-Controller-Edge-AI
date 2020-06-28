be368f19875698cfe514495cd6b8857a938b98bc
yesusbc <yesus.becerril@gmail.com>
2020-06-27 22:47:43 -0500

    Landmarks detection added

---
+++ src/facial_landmarks_detection.py
@@ -0,0 +1,123 @@
+"""
+Base class for landmarks detection Model
+landmarks-regression-retail-0009
+Name: "data" , shape: [1x3x48x48] - An input image in the format [BxCxHxW]
+
+The net outputs a blob with the shape: [1, 10], containing a row-vector of 10 floating point values for five landmarks
+coordinates in the form (x0, y0, x1, y1, ..., x5, y5). All the coordinates are normalized to be in range [0,1].
+"""
+import sys
+import logging as log
+from openvino.inference_engine import IECore
+import cv2
+import numpy as np
+
+EXTENSIONS_PATH = "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so"
+LANDMARKS_MODEL_PATH = "../intel_models/landmarks-regression-retail-0009/FP32/landmarks-regression-retail-0009"
+
+
+class LandmarksDetectionModel:
+    """
+    Class for the Face Detection Model.
+    """
+    def __init__(self, model_path=LANDMARKS_MODEL_PATH, device="CPU", extensions=None):
+        """
+        Set instance variables.
+        """
+        self.ie = IECore()
+        self.net = None
+        self.exec_net = None
+
+        self.model_weights = model_path+".bin"
+        self.model_structure = model_path+".xml"
+        self.device = device
+        self.extensions = extensions
+
+        try:
+            # Read the IR as an IENetwork
+            self.net = self.ie.read_network(self.model_structure, self.model_weights)
+        except Exception as e:
+            raise ValueError("Could not Initialise the network. Have you entered the correct model path? \
+                            Check for {0}".format(e))
+
+        self.input_name = next(iter(self.net.inputs))
+        self.input_shape = self.net.inputs[self.input_name].shape
+        self.output_name = next(iter(self.net.outputs))
+        self.output_shape = self.net.outputs[self.output_name].shape
+        self.w = None
+        self.h = None
+
+    def load_model(self):
+        """
+        Check for supported layers and add extensions if necessary
+        Initialize Inference Engine, to work with the plugin
+        Load the IENetwork into the plugin
+        """
+
+        # Add any necessary extension
+        if self.extensions and self.device == "CPU":
+            self.ie.add_extension(self.extensions, self.device)
+            self.ie.add_extension(extension_path=EXTENSIONS_PATH, device_name=self.device)
+
+        # Get the supported layers of the network
+        layers_map = self.ie.query_network(network=self.net, device_name=self.device)
+
+        # Look for unsupported layers
+        unsupported_layers = [layer for layer in self.net.layers.keys() if layer not in layers_map]
+
+        # If there're unsupported layers, notify and exit
+        if len(unsupported_layers):
+            log.error("There were unsupported layers on the network, try checking if path \
+                      on --cpu_extension is correct. The unsupported layers were: {0}\
+                      ".format(unsupported_layers))
+            sys.exit(1)
+
+        # Load the IENetwork into the plugin
+        self.exec_net = self.ie.load_network(self.net, self.device, num_requests=1)
+
+    def predict(self, image):
+        """
+        Make inference over the exectutable network
+        """
+        self.w = image.shape[1]
+        self.h = image.shape[0]
+        p_frame = self.preprocess_input(image)
+        outputs = self.exec_net.infer({self.input_name : p_frame})
+        left_eye, right_eye = self.preprocess_outputs(outputs[self.output_name])
+
+        # Left eye coords and cropped image
+        y_left_eye = int(left_eye[1])
+        x_left_eye = int(left_eye[0])
+        cropped_left_eye = image[(y_left_eye-15):(y_left_eye+15), (x_left_eye-15):(x_left_eye+15)]
+
+        # Right eye coords and cropped image
+        y_right_eye = int(right_eye[1])
+        x_right_eye = int(right_eye[0])
+        cropped_right_eye = image[(y_right_eye-15):(y_right_eye+15), (x_right_eye-15):(x_right_eye+15)]
+
+        # coords from every eye in the form [[eye1_coords], [eye2_coords]]
+        eyes_coords = [[(x_left_eye-15, y_left_eye-15), (x_left_eye+15, y_left_eye+15)],
+                       [(x_right_eye-15, y_right_eye-15), (x_right_eye+15, y_right_eye+15)]]
+
+        return cropped_left_eye, cropped_right_eye, eyes_coords
+
+    def preprocess_input(self, image):
+        """
+        Given an input image, height and width:
+        - Resize to height and width
+        - Transpose the final "channel" dimension to be first
+        - Reshape the image to add a "batch" of 1 at the start
+        """
+        p_frame = cv2.resize(image, (self.input_shape[3], self.input_shape[2]))
+        p_frame = p_frame.transpose((2, 0, 1))
+        p_frame = p_frame.reshape(1, *p_frame.shape)
+        return p_frame
+
+    def preprocess_outputs(self, landmarks_vector):
+        """
+        This function applies a probability threshold to the output data.
+        The return will contain the related coordinates of the prediction, resized to the original image size
+        """
+        left_eye = (landmarks_vector[0][0][0][0]*self.w, landmarks_vector[0][1][0][0]*self.h)
+        right_eye = (landmarks_vector[0][2][0][0]*self.w, landmarks_vector[0][3][0][0]*self.h)
+        return left_eye, right_eye
---
+++ src/main.py
@@ -0,0 +1,60 @@
+"""
+Main File
+"""
+
+from face_detection import FaceDetectionModel
+from facial_landmarks_detection import LandmarksDetectionModel
+from input_feeder import InputFeeder
+from mouse_controller import MouseController
+import argparse
+import cv2
+
+def get_args():
+    '''
+    Gets the arguments from the command line.
+    '''
+    parser = argparse.ArgumentParser("Basic Edge App with Inference Engine")
+
+    c_desc = "CPU extension file location, if applicable"
+    d_desc = "Device, if not CPU (GPU, FPGA, MYRIAD)"
+    i_desc = "The location of the input file"
+    mface_desc = "The location of the face model path"
+    p_desc = "Probability threshold"
+    it_desc = "Input Type, video, cam, or image"
+
+    parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
+
+    required.add_argument("-i", help=i_desc, required=True)
+    required.add_argument("-m", help=mface_desc, required=True)
+    required.add_argument("-it", help=it_desc, required=True)
+    optional.add_argument("-c", help=c_desc, default=None)
+    optional.add_argument("-d", help=d_desc, default="CPU")
+    optional.add_argument("-p", help=p_desc, default="0.6")
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = get_args()
+
+    feed = InputFeeder(input_type=args.it, input_file=args.i)
+
+    face_model = FaceDetectionModel()    # FaceDetectionModel(args.m, args.d, args.c,args.p)
+    face_model.load_model()
+
+    landmarks_model = LandmarksDetectionModel()
+    landmarks_model.load_model()
+
+    feed.load_data()
+    for batch in feed.next_batch():
+        cropped_face, coords = face_model.predict(batch)
+        landmarks_model.predict(cropped_face)
+        break
+    feed.close()
+
+
+if __name__ == "__main__":
+    main()
